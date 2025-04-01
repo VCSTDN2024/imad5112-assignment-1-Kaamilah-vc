44997f4f39287867c8a8fff2a95b33a42ac36011
Kaamilah-vc <ST10492062@vcconnect.edu.za>
2025-04-01 16:30:45 +0200

    Added references to mainActivity.kt

--- app/src/main/java/vcmsa/ci/mealsuggestion/MainActivity.kt
+++ app/src/main/java/vcmsa/ci/mealsuggestion/MainActivity.kt
@@ -1,4 +1,13 @@
 package vcmsa.ci.mealsuggestion
+// REFERENCE LIST
+ // Kotlin for developers. 2024. “Write Conditionals in Kotlin  |  Android Developers”.
+// Android Developers. 2024 <https://developer.android.com/codelabs/basic-android-kotlin-compose-conditionals#0>
+// [accessed 1 April 2025].
+
+// Kincade Garanganga. 2024. “IMAD5112 Practicum Discussion 2024”. YouTube. December 7, 2024
+// <https://www.youtube.com/watch?v=_u8zA1CxxV4>
+// [accessed 1 April 2025].
+
 
 import android.os.Bundle
 import android.widget.Button
@@ -21,13 +30,13 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
+//(Kotlin for developers 2024)
         // linking current activity variables to UI components
         mealText = findViewById(R.id.mealText)
         buttonMeal = findViewById(R.id.buttonMeal)
         displaymeal = findViewById(R.id.displaymeal)
         resetbutton = findViewById(R.id.resetbutton)
-
+       // (Kotlin for developers 2024)
         buttonMeal.setOnClickListener{ // button used to display food items in textview
             handleButtonMealClick()
         }
@@ -35,20 +44,21 @@ class MainActivity : AppCompatActivity() {
             handleResetButtonClick() // button used to clear food items in textview
         }
     }
-
+//(Kotlin for developers 2024)
     // method logic to clear textview
      private fun handleResetButtonClick(){
          displaymeal.text = ""
      }
     private fun handleButtonMealClick(){ // fuction used to enter the time of a day
         val mealInput = mealText.text.toString().trim().lowercase()
-
+//(Kotlin for developers 2024)
         if (mealInput.isEmpty()) {
             displaymeal.text = "Enter the time of day"
             return
         }
 
         try {
+            //(Kotlin for developers 2024)
             displaymeal.text = when (mealInput.lowercase()) { // case statement used to check and suggest a meal according to the time
                 "morning" -> "Pancakes, Omelette, Smoothie"
                 "mid-morning" -> "Muffins, Fruit Salad, Biscuits"
